9eba690b3dbcf4a1e4307d430e614806acf14615
yujinii59 <wssj3259@naver.com>
2023-04-14 16:21:57 +0900

    add the print method
    특정 repository 정보 출력

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from request import account
+from request import repo
+
+import asyncio
 
 user = account.User()
 username = user.get_username()
 print(username)
+
+data = asyncio.run(repo.get_repository_info(username, "github-repository-information-service"))
+print(data)
