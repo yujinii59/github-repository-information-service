c791a27c1357142714dff499205d7f4ef585c2ca
yujinii59 <wssj3259@naver.com>
2023-04-15 02:03:33 +0900

    Edit error message
    404 에러 메시지 수정 #4

--- error.py
+++ error.py
@@ -1,16 +1,20 @@
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, NotFound
 from exception.custom_exception import TokenException, NotExistException
 
 
 def error_handler(app):
-    @app.errorhandler(HTTPException)
-    def handle_bad_request(e):
-        return 'error!!!!!!!!!', e.code
 
+    # access with wrong url
+    @app.errorhandler(NotFound)
+    def handle_not_found(e):
+        return 'This url format is wrong. Insert the url formated : <127.0.0.1:5000/<naming list seperated by comma>', e.code
+
+    # the access token is wrong or not found
     @app.errorhandler(TokenException)
     def handle_token_error(e):
         return e.dev_error_message, e.status_code
 
+    # some repository's name in endpoints doesn't exist
     @app.errorhandler(NotExistException)
     def handle_token_error(e):
         return e.dev_error_message, e.status_code
