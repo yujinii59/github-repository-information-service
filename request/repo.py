ef44ef7e5c3caf20be91e1e9cf11598a7fd637ba
yujinii59 <wssj3259@naver.com>
2023-04-14 23:19:37 +0900

    Reformat result
    원하는 정보에 맞춰 결과 reformat #2

--- request/repo.py
+++ request/repo.py
@@ -27,9 +27,19 @@ async def get_repository_info(username, repo):
             """
         }
         async with session.post('https://api.github.com/graphql', headers=headers, data=json.dumps(data)) as response:
-            print("Status:", response.status)
-            html = await response.text()
-            data = json.loads(html).get('data', {})
-            print('data: ', data)
 
-            return data
+            html = await response.text()
+            data = json.loads(html).get('data', {}).get('repository', {})
+            if data:
+                result = {
+                    'Repository name': data['name'],
+                    'Repository description': data['description'],
+                    'Number of open issues': data['issueCount']['totalCount'],
+                    'A list of the 5 most recent issue titles': data['last5Issues']['nodes']
+                }
+            else:
+                result = {
+                    'Repository name': repo,
+                    'Error description': "This name is not a correct name."
+                }
+            return result
