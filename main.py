00c1a256651c607074169de30aa52674be6778d8
yujinii59 <wssj3259@naver.com>
2023-04-14 23:28:15 +0900

    Change one to many
    여러 repository name을 리스트로 받아 비동기적으로 처리하는 것으로 변경 #2

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ user = account.User()
 username = user.get_username()
 print(username)
 
-data = asyncio.run(repo.get_repository_info(username, "github-repository-information-service"))
+data = asyncio.run(repo.get_repositories_info(username, ["github-repository-information-service"]))
 print(data)
--- request/repo.py
+++ request/repo.py
@@ -1,45 +1,55 @@
 import aiohttp
 import json
+import asyncio
 
 import common
 
 headers = common.github_graphql_header
 
 
-async def get_repository_info(username, repo):
-    async with aiohttp.ClientSession() as session:
-        data = {
-            "query": f"""
-                query {{
-                    repository(owner: "{username}", name: "{repo}") {{
-                        name
-                        description
-                        issueCount: issues(states:OPEN) {{
-                            totalCount
-                        }}
-                        last5Issues: issues(last:5) {{
-                            nodes {{
-                                    title
-                            }}
+async def get_repository_info(session, username, repo):
+    data = {
+        "query": f"""
+            query {{
+                repository(owner: "{username}", name: "{repo}") {{
+                    name
+                    description
+                    issueCount: issues(states:OPEN) {{
+                        totalCount
+                    }}
+                    last5Issues: issues(last:5) {{
+                        nodes {{
+                                title
                         }}
                     }}
                 }}
-            """
-        }
-        async with session.post('https://api.github.com/graphql', headers=headers, data=json.dumps(data)) as response:
+            }}
+        """
+    }
+    async with session.post('https://api.github.com/graphql', headers=headers, data=json.dumps(data)) as response:
+        print("Status:", response.status)
+        html = await response.text()
+        json_data = json.loads(html)
+        data = json_data.get('data', {}).get('repository', {})
+        print('data: ', data)
+        if data:
+            result = {
+                'Repository name': data['name'],
+                'Repository description': data['description'],
+                'Number of open issues': data['issueCount']['totalCount'],
+                'A list of the 5 most recent issue titles': data['last5Issues']['nodes']
+            }
+        else:
+            result = json_data
+            return {"error":"repository not found",
+                    "Repository name": repo}
+        return result
 
-            html = await response.text()
-            data = json.loads(html).get('data', {}).get('repository', {})
-            if data:
-                result = {
-                    'Repository name': data['name'],
-                    'Repository description': data['description'],
-                    'Number of open issues': data['issueCount']['totalCount'],
-                    'A list of the 5 most recent issue titles': data['last5Issues']['nodes']
-                }
-            else:
-                result = {
-                    'Repository name': repo,
-                    'Error description': "This name is not a correct name."
-                }
-            return result
+
+async def get_repositories_info(username, names):
+    async with aiohttp.ClientSession() as session:
+        results = await asyncio.gather(
+            *[get_repository_info(session, username, name) for name in names]
+        )
+
+        return results
