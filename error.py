a4569f3dcae1db1fb155bb322bd0e8c8d5f1127b
yujinii59 <wssj3259@naver.com>
2023-04-15 15:44:22 +0900

    Add Some error exception
    생길 수 있는 에러 핸들링

--- error.py
+++ error.py
@@ -1,8 +1,28 @@
-from werkzeug.exceptions import InternalServerError, NotFound
+from werkzeug.exceptions import BadRequest, Forbidden, NotFound, RequestTimeout, InternalServerError
 from exception.custom_exception import TokenException, NotExistException
 
 
 def error_handler(app):
+    @app.errorhandler(BadRequest)
+    def handle_bad_request(e):
+        """
+        Raise if the browser sends something to the application or server cannot handle.
+        :param e: error object
+        :return: error message and status code
+        """
+
+        return "<h2>Bad Request</h2>" + e.description, e.code
+
+    @app.errorhandler(Forbidden)
+    def handle_permission_error(e):
+        """
+        no permission for the requested resource
+        :param e: error object
+        :return: error message and status code
+        """
+
+        return "<h2>Permission Denied</h2>" + e.description, e.code
+
     @app.errorhandler(NotFound)
     def handle_not_found(e):
         """
@@ -11,7 +31,19 @@ def error_handler(app):
         :return: error message and status code
         """
 
-        return 'This url format is wrong. Insert the url formated : <127.0.0.1:5000/<naming list seperated by comma>', e.code
+        return "<h2>Not Found</h2>" \
+               "This url format is wrong.<br>" \
+               "Insert the url formated : http://127.0.0.1:5000/api/repository-name1,repository-name2,...", e.code
+
+    @app.errorhandler(RequestTimeout)
+    def handle_timeout(e):
+        """
+        timeout
+        :param e: error object
+        :return: error message and status code
+        """
+
+        return "<h2>Timeout</h2>" + e.description, e.code
 
     @app.errorhandler(InternalServerError)
     def handle_sever_error(e):
@@ -21,7 +53,7 @@ def error_handler(app):
         :return: error message and status code
         """
 
-        return e.description, e.code
+        return "<h2>Server Error</h2>"+e.description, e.code
 
     @app.errorhandler(TokenException)
     def handle_token_error(e):
