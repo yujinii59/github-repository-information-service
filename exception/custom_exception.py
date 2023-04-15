2da469899fa91930cb285a9a36de7502421da847
yujinii59 <wssj3259@naver.com>
2023-04-15 15:42:46 +0900

    Change status code
    error status code 수정

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,11 +1,14 @@
 class TokenException(Exception):
     def __init__(self):
-        self.status_code = 600
+        self.status_code = 401
         self.dev_error_message = \
-            "Check the github personal access token. \nIf you didn't create an environment variable, please create it."
+            "<h2>GitHub Access Token Error</h2>" \
+            "Check the github personal access token. <br>" \
+            "If you didn't create an environment variable, please create it."
 
 
 class NotExistException(Exception):
     def __init__(self):
-        self.status_code = 601
-        self.dev_error_message = "Some repositories doesn't exist"
+        self.status_code = 406
+        self.dev_error_message = "<h2>Repository name Error</h2>" \
+                                 "Some repositories doesn't exist."
